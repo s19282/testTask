0b3958619c6d14a97129d774a6a28b08983a2368
mateusz <s19282@pjwstk.edu.pl>
2022-03-20 23:05:08 +0100

    parametrized type of list

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,32 @@
 package com.company;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
+    static List<Test> myList = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException {
         System.out.println("Hello world!");
         Test t = new Test();
-        System.out.println(Arrays.toString(t.getClass().getInterfaces()));
-        System.out.println(Arrays.toString(t.getClass().getInterfaces()[0].getInterfaces()[0].getDeclaredMethods()));
-        System.out.println(Arrays.toString(t.getClass().getInterfaces()[0].getDeclaredMethods()));
+        System.out.println(myList.getClass().getComponentType());
+
+//        System.out.println(Arrays.toString(t.getClass().getDeclaredMethods()));
+//        System.out.println(t.getClass().getMethod("getBlocks"));
+//        t.getClass().getMethod("getBlocks");
+        try {
+            Field integerListField = Main.class.getDeclaredField("myList");
+            ParameterizedType integerListType = (ParameterizedType) integerListField.getGenericType();
+            System.out.println(integerListType);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+//        myList.getClass().getMethod("getBlocks");
+//        System.out.println(Arrays.toString(t.getClass().getInterfaces()));
+//        System.out.println(Arrays.toString(t.getClass().getInterfaces()[0].getInterfaces()[0].getDeclaredMethods()));
+//        System.out.println(Arrays.toString(t.getClass().getInterfaces()[0].getDeclaredMethods()));
     }
 }
--- src/com/company/Wall.java
+++ src/com/company/Wall.java
@@ -1,18 +1,28 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
 import java.util.Optional;
 
 public class Wall implements Structure {
     private List blocks;
 
+    public Wall(List blocks) {
+        this.blocks = blocks;
+    }
+
     @Override
     public Optional findBlockByColor(String color) {
-//        List matchColourBlocks;
-////        List<Class<? extends CompositeBlock>> list = new ArrayList<>();
-//        blocks.getClass().getInterfaces()
+        List matchColourBlocks;
+        try {
+            Field integerListField = Wall.class.getDeclaredField("blocks");
+            ParameterizedType integerListType = (ParameterizedType) integerListField.getGenericType();
+            System.out.println(integerListType);      }catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+//        List<Class<? extends CompositeBlock>> list = new ArrayList<>();
+//        blocks.getClass().getInterfaces()[0].getMethod("getBlocks")
 //        for (Class<? extends CompositeBlock> el : blocks)
 //        blocks.forEach(compositeBlock -> compositeBlock.getBlocks().forEach(block -> {
 //            if(block.getColor().equals(color)) matchColourBlocks.add(block);
