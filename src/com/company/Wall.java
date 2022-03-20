8cdecd95a943104826c091b14c02fee42cdd3305
mateusz <s19282@pjwstk.edu.pl>
2022-03-20 21:56:43 +0100

    first thought

--- src/com/company/Wall.java
+++ src/com/company/Wall.java
@@ -8,16 +8,31 @@ public class Wall implements Structure {
 
     @Override
     public Optional findBlockByColor(String color) {
-        return Optional.empty();
+        List matchColourBlocks;
+        blocks.forEach(compositeBlock -> compositeBlock.getBlocks().forEach(block -> {
+            if(block.getColor().equals(color)) matchColourBlocks.add(block);
+        }));
+        if(matchColourBlocks.isEmpty())
+            return Optional.empty();
+        else
+            return Optional.of(matchColourBlocks);
     }
 
     @Override
     public List findBlocksByMaterial(String material) {
-        return null;
+        List matchMaterialBlocks;
+        blocks.forEach(compositeBlock -> compositeBlock.getBlocks().forEach(block -> {
+            if(block.getMaterial().equals(material)) matchMaterialBlocks.add(block);
+        }));
+        return matchMaterialBlocks;
     }
 
     @Override
     public int count() {
-        return 0;
+        int counter=0;
+        for (Object compositeBlock : blocks) {
+            counter+=compositeBlock.getBlocks().size();
+        }
+        return counter;
     }
 }
